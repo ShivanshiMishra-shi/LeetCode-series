ad272ac7a6ae33fe7019746ca85df2ae79d062c0
ShivanshiMishra-shi <shivanshimishra6788@gmail.com>
2026-08-16 19:33:04 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (79.10%) - LeetSync

---
+++ 100-same-tree/same-tree.c
@@ -0,0 +1,22 @@
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+
+    if(p == NULL && q == NULL)
+    {
+        return true;
+    }
+
+    if(p == NULL || q == NULL)
+    {
+        return false;
+    }
+
+    if(p->val != q->val)
+    {
+        return false;
+    }
+
+    bool left = isSameTree(p->left, q->left);
+    bool right = isSameTree(p->right, q->right);
+
+    return left && right;
+}
