b36c281293f1431eb774286d98d8b6499303eb1d
ShivanshiMishra-shi <shivanshimishra6788@gmail.com>
2026-09-02 13:39:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (46.06%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.c
@@ -0,0 +1,22 @@
+int singleNonDuplicate(int* nums, int numsSize) {
+    int l=0;
+    int h=numsSize-1;
+    while(l<h)
+    {
+        int mid=l+(h-l)/2;
+        if(mid % 2==1)
+        {
+            mid--;
+        }   
+        if(nums[mid]==nums[mid+1])
+        {
+            l=mid+2;
+        }
+        else
+        {
+            h=mid;
+        }
+    }
+    return nums[l];
+    
+}
